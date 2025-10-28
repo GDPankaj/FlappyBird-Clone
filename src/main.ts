5451c0fce8f37b2c0105fd79ecfabfab9db7751d
Pankaj <Pankajsawant2258@gmail.com>
2025-10-28 19:25:08 +0530

    space button interaction

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { Application, Assets, Sprite } from "pixi.js";
 
-(async () => {
+( async () => {
   // Create a new application
   const app = new Application();
 
   // Initialize the application
-  await app.init({ background: "#1099bb", resizeTo: window });
+  await app.init({ width : 320, height : 240, background: "#1099bb"});
 
   // Append the application canvas to the document body
   document.getElementById("pixi-container")!.appendChild(app.canvas);
@@ -14,22 +14,38 @@ import { Application, Assets, Sprite } from "pixi.js";
   const texture = await Assets.load("/assets/bunny.png");
 
   // Create a bunny Sprite
-  const bunny = new Sprite(texture);
+  const bunny: Sprite = new Sprite(texture);
 
   // Center the sprite's anchor point
   bunny.anchor.set(0.5);
 
   // Move the sprite to the center of the screen
   bunny.position.set(app.screen.width / 2, app.screen.height / 2);
-
+  const gravity = 0.1;
+  var speed = 0;
+    const minimumY = app.screen.height
+    const groundY = app.screen.height - app.screen.height/2;
   // Add the bunny to the stage
-  app.stage.addChild(bunny);
-
+    app.stage.addChild(bunny);
+    window.addEventListener('keydown', (e) => {
+        if (e.code === 'Space') {
+            speed += -5;
+        }
+    });
   // Listen for animate update
   app.ticker.add((time) => {
     // Just for fun, let's rotate mr rabbit a little.
     // * Delta is 1 if running at 100% performance *
     // * Creates frame-independent transformation *
-    bunny.rotation += 0.1 * time.deltaTime;
+      //bunny.rotation += 0.1 * time.deltaTime;
+      speed += gravity * time.deltaTime;
+      bunny.position.y += speed;
+      if (bunny.position.y > app.screen.height) {
+          bunny.position.y = app.screen.height;
+          speed *= -0.6; // bounce
+      }
+      if (bunny.position.y < 0) {
+          bunny.position.y = 0;
+      }
   });
 })();
